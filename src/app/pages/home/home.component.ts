8455473900c5e44ef6020aa6667752e180079d5e
matthiasmohr <mail@mohr-matthias.de>
2025-01-14 19:06:00 +0100

    Update Referenzen

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import {environment} from "../../../environments/environment.stage";
 import {ContactApiService} from "../../shared/contact.service";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
